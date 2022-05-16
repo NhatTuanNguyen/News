7b0a215cbb1a24fc1b3767345dbab92f00d6dd88
NhatTuanNguyenUIT <nhattuanvip@gmail.com>
2022-05-16 19:53:56 +0700

    test

--- app/routes/frontEnd/generalNews.js
+++ app/routes/frontEnd/generalNews.js
@@ -13,22 +13,30 @@ const layoutBlog = __path_views_blog + 'frontend';
 
 /* GET home page. */
 router.get('/:slug', async function (req, res, next) {
-  let linkRss =''
+  let linkRss ='';
   let slugGeneralNews = paramsHelper.getParams(req.params, 'slug', '');
 
   await generalNewsModel.getItemsFromSlug(slugGeneralNews).then((items) => {
     linkRss = items[0].link;
   });
-  let feed = await parser.parseURL(linkRss);
-  
-  feed['items'].forEach((item)=>{
-    item.linkImage = parserHtml.parse(item.content).querySelector('img').getAttribute('src');
-  });
-
-  res.render(`${folderView}index`, {
-    layout: layoutBlog,
-    top_post: false,
-    items:feed.items,
+  parser.parseURL(linkRss,(err, feed) => {
+    if(err){
+      res.render(__path_views_blog + 'pages/error', {
+        pageTitle: 'Page Not Found',
+        top_post:false,
+        layout: false,
+      });
+    } else{
+      feed['items'].forEach((item)=>{
+        item.linkImage = parserHtml.parse(item.content).querySelector('img').getAttribute('src');
+      });
+    
+      res.render(`${folderView}index`, {
+        layout: layoutBlog,
+        top_post: false,
+        items:feed.items,
+      });
+    }
   });
 });
 
