c8ca5015b8d413ccb83b493b1ae157d78c8af910
NhatTuanNguyenUIT <nhattuanvip@gmail.com>
2022-06-15 18:58:37 +0700

    make it better

--- app/configs/system.js
+++ app/configs/system.js
@@ -1,7 +1,7 @@
 module.exports = {
     prefixAdmin: 'admin1ff23',
     prefixBlog: '',
-    env:'dev',
+    env:'production',
     formatLongTime: 'HH:mm DD-MM-YYYY',
     formatDayTime: 'DD-MM-YYYY',
 }
